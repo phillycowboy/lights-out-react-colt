9e070566d80fae6225b5a39cd34545d2f2178154
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-04 14:48:34 -0400

    finished

--- src/Board.js
+++ src/Board.js
@@ -63,6 +63,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log("FLIPPING", coord);
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -75,20 +76,52 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
+    
+    flipCell(y,x); // flip initial cell 
+    flipCell(y, x - 1); //flip left 
+    flipCell(y, x + 1); //flip right 
+    flipCell(y - 1, x); //flip below
+    flipCell(y + 1, x); //flip above
 
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-
-    // this.setState({board, hasWon});
+    let hasWon = board.every(row => row.every(cell => !cell));
+    this.setState({board: board, hasWon: hasWon});
   }
 
 
   /** Render game board or winning message. */
+  makeTable(){
+    let tblBoard = [];
+    for(let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++){
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]}
+        flipCellsAroundMe={() => this.flipCellsAround(coord)}/>)
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
+    return(
+      <table className='Board'>
+      <tbody>{tblBoard}</tbody>
+    </table>
+    )
+  }
 
   render() {
-
+    // if(this.state.hasWon){
+    //   return(
+      //   <div className="Board-title">
+      //     <div className="winner">
+      //       <span className="neon-orange">YOU</span>
+      //       <span className="neon-blue">WIN!</span>
+      //     </div>
+      // </div>
+    //   )
+    // }
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
@@ -96,22 +129,28 @@ class Board extends Component {
     // make table board
 
     // TODO
-    let tblBoard = [];
-    for(let y = 0; y < this.props.nrows; y++){
-      let row = [];
-      for(let x = 0; x < this.props.ncols; x++){
-        let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>)
-      }
-      tblBoard.push(<tr key={y}>{row}</tr>)
-    }
 
     return(
-      <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
-      </table>
+      
+      <div>
+        <div>
+          {this.state.hasWon ? (
+            <div className='winner'>
+              <span className='neon-orange'>YOU</span>
+              <span className='neon-blue'>WIN!</span>
+            </div>
+        ) : (
+        <div>
+            <div className='Board-title'>
+              <div className='neon-orange'>Lights</div>
+              <div className='neon-blue'>Out</div>
+            </div>
+            {this.makeTable()}
+        </div>
+        )}
+        </div>
+      </div>
+
     )
   }
 }
